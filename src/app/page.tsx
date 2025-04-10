859bf477588bb3502c96d409c43f20719e31178b
ssyaa <aulputri4877@gmail.com>
2025-04-10 11:47:16 +0800

    perbaikan dikit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,16 @@
+// src/app/page.tsx
+import About from "@/app/about/page";
+import Contact from "@/app/contact/page";
+import Project from "@/app/projects/page";
+import Experience from "@/app/experience/page";
 
-import type { Metadata } from "next";
-import { ReactNode } from "react";
-import Navbar from "@/app/components/Navbar";
-import Footer from "@/app/components/Footer";
-import About from "@/app/about/page"
-import Contact from "@/app/contact/page"
-import Project from "@/app/projects/page"
-import Experience from "@/app/experience/page"
-
-
-export const metadata: Metadata = {
-  title: "Tasya Aulia Putri",
-  description: "Portofolio pribadi saya",
-};
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function HomePage() {
   return (
     <div>
-        <Navbar />
-        <About />
-        <Experience/>
-        <Project />
-        <Contact />
-        <Footer />
+      <About />
+      <Experience />
+      <Project />
+      <Contact />
     </div>
   );
 }
---
+++ src/app/skill/page.tsx
@@ -0,0 +1,9 @@
+export default function SkillPage() {
+    return (
+      <div>
+        <h1>Skill</h1>
+        <p>Isi skill kamu di sini, Tasya~ ✨</p>
+      </div>
+    );
+  }
+  
